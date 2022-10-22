b04edf0e260b0b3ccbe76515d2a2677865f6f129
Sachin Burnawal <sachinburnawal01@gmail.com>
2022-10-22 13:09:57 +0530

    changes

---
+++ a.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+import xmlrpc.client
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# in local
+db = "aamer_151"  # Database name
+odoo_username = "admin"  # odoo username
+odoo_password = "admin"  # odoo password
+url = 'http://localhost:2365' # odoo url/domain can work with https
+# end
+
+common = xmlrpc.client.ServerProxy(url+"/xmlrpc/2/common")
+uid = common.authenticate(db, odoo_username, odoo_password, {})
+models = xmlrpc.client.ServerProxy("{}/xmlrpc/2/object".format(url))
+
+
+payment_ids = models.execute_kw(
+        db,
+        uid,
+        odoo_password,
+        "account.payment",
+        "search_read",
+        [],
+        {
+            'fields': ['id', 'payment_type'],
+            # 'offset': offset*1000,
+            # 'limit': 20
+            'order': 'id asc'
+        })
+for s in payment_ids:
+    payment_type = s.get('payment_type', '')
+    code = ''
+    if payment_type == 'inbound':
+        code = 'account.payment.customer.invoice'
+    elif payment_type == 'outbound':
+        code = 'account.payment.supplier.invoice'
+
+    payment_seq = models.execute_kw(db, uid, odoo_password, "ir.sequence", 'next_by_code', [], {
+        'sequence_code': code
+    })
+    models.execute_kw(db, uid, odoo_password, "account.payment", 'write', [[s['id']], {
+        'sltech_name': payment_seq
+    }])
+    print("Updated Payment Sequence: "+str(s))
